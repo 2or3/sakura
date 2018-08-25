727bf610797f0212595a7fbe34d14bcb30158a50
2or3 <t.nagashima.cog@gmail.com>
2018-08-25 22:02:56 +0900

    fix html

--- parts/greeting.php
+++
@@ -1,4 +0,0 @@
-          <h3><span class="label glyphicon glyphicon-leaf">ごあいさつ</span><span class="label label-default">Greeting</span></h3>
-          <p style="border-bottom-style: outset;"></p>
-          <img class="img-responsive" src="./assets/lib/images/photos/greeting.jpg"/>
-
